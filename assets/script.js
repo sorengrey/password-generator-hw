8dcdb9e3259db848b599e9941c6fc6853de6cc6f
sorengrey <65551656+sorengrey@users.noreply.github.com>
2020-12-29 13:04:07 -0500

    Updates to password generator function
    generatePassword function now returns the proper amount of characters selected by the user.

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,6 @@
 // attaches the generate button to the variable generateBtn
 var generateBtn = document.querySelector("#generate");
 
-// attaches the hidden element to the variable hiddenElement -- might not need this
-//var hiddenElement = document.querySelector("#hidden");
-
 // writes generated password into the #password input field
 function writePassword() {
   var password = generatePassword();
@@ -24,24 +21,38 @@ function showForm() {
     document.getElementById("hidden").style.display = "block";
 }
 
- // generates a random 8-character password with letters, numbers, and special characters
- function generatePassword() {
-  var length = document.querySelector("#passwordlength").value,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-      specialChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-='",
-      returnedPw = "";
-      returnedSp = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      returnedPw += charset.charAt(Math.floor(Math.random() * n));
+ // generates a random password with lowercase letters -- do not touch!!
+ /* function generatePassword() {
+  var pwlength = document.querySelector("#passwordlength").value,
+      lowercase = "abcdefghijklmnopqrstuvwxyz",
+      uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      numerical = "0123456789",
+     specialChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-='",
+     returnedPw = " ";
+    // returnedSp = " ";
+  for (var i = 0, n = lowercase.length; i < pwlength; ++i) {
+      returnedPw += lowercase.charAt(Math.floor(Math.random() * n));
   }
-  for (var i = 0, n = specialChars.length; i < length; ++i) {
-      returnedSp += specialChars.charAt(Math.floor(Math.random() * n));
-  }
-  var joinedSets = (returnedPw + returnedSp);
-  var shuffled = joinedSets.split('').sort(function(){return 0.5-Math.random()}).join('');
-  return shuffled;
+ // for (var i = 0, n = uppercase.length; i < pwlength; ++i) {
+  //   returnedSp += uppercase.charAt(Math.floor(Math.random() * n));
+ // }
+  //var joinedSets = (returnedPw + returnedSp);
+  //var shuffled = joinedSets.split('').sort(function(){return 0.5-Math.random()}).join('');
+  return returnedPw;
 }
+*/
 
+// testing - generates a random password with the right amount of characters, but needs checkbox logic
+function generatePassword() {
+    var pwlength = document.querySelector("#passwordlength").value,
+    characters= "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "!@#$%^&*()_+~`|}{[]\:;?><,./-='",
+       returnedPw = " ";
+      // returnedSp = " ";
+    for (var i = 0, n = characters.length; i < pwlength; ++i) {
+        returnedPw += characters.charAt(Math.floor(Math.random() * n));
+    }
+    return returnedPw;
+  }
 
 /*  UI adjustments needed:
 - Error message if you don't click a checkbox "Please select at least one character type." - validation
